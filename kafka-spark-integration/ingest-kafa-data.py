07ceb2bf84df31a3175b0651ee6bc208da3b648c
fogofortitude <nick.drewitt.smith@gmail.com>
2020-09-16 19:16:28 +1000

    Added local host port for ZooKeeper line 10

--- kafka-spark-integration/ingest-kafa-data.py
+++ kafka-spark-integration/ingest-kafa-data.py
@@ -1,21 +1,24 @@
 import logging
 from pyspark.sql import SparkSession
 
+
 def run_spark_job(spark):
     #TODO set this entry point so that you can start ingesting kafka data
     df = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:<your port>") \
-        .option("subscribe", "<your topic name>") \
+        .option("kafka.bootstrap.servers", "localhost:2181") \
+        .option("subscribe", "uber.event.pickup") \
         .option("startingOffsets", "earliest") \
         .option("maxOffsetsPerTrigger", 10) \
         .option("stopGracefullyOnShutdown", "true") \
         .load()
 
+
     # Show schema for the incoming resources for checks
     df.printSchema()
-    
+
+
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
@@ -27,6 +30,5 @@ if __name__ == "__main__":
 
     logger.info("Spark started")
 
-    run_spark_job(spark)
 
     spark.stop()
