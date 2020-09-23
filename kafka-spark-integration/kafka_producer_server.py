50f844139635e1c4f89e6aca241c9e38c0b0786f
fogofortitude <nick.drewitt.smith@gmail.com>
2020-09-23 10:23:30 +1000

    Update kafka_producer_server.py
    Added comment for each function within the class. lines 26 and 13
    Added print statement for testing purposes on line 15
    Reassigned the input values from file to a new attribute called data on line 17
    Altered for loop to point to the new data attribute created - line 19

--- kafka-spark-integration/kafka_producer_server.py
+++ kafka-spark-integration/kafka_producer_server.py
@@ -9,14 +9,21 @@ class ProducerServer(KafkaProducer):
         super().__init__(**kwargs)
         self.input_file = input_file
         self.topic = topic
-
+    
+    # generates dummy data from input file
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
+        print(self.topic)
+        with open(self.input_file) as file:
+            data = json.load(file)
+            
+            for line in data:
                 message = self.dict_to_binary(line)
+                data = json.load(file)
+                # send the message data for the topic
                 self.send(self.topic, message)
                 time.sleep(1)
 
+    # returns the json dictionary to binary               
     def dict_to_binary(self, json_dict):
         return json.dumps(json_dict).encode('utf-8')
 
